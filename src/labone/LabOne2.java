7f8115e669bfc46b6421fcff2f2b548adc9a0818
no-a3 <sin.h33990@gmail.com>
2021-09-11 17:14:48 +0300

    first commit

---
+++ src/labone/LabOne2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labone;
+
+/**
+ *
+ * @author USER
+ */
+public class LabOne2 {
+    
+    public void sum(int int1, int int2){
+        
+        System.out.println(int1+int2);
+    }
+}
